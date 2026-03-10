a520f0c7e6e7e7050c2590cef9e36305ceb1b969
ys276854 <ys7981@srmist.edu.in>
2026-03-10 09:18:37 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,21 @@
+/**
+ * Book My Stay App
+ * Demonstrates the entry point of the Hotel Booking application.
+ *
+ * @author Student
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("        BOOK MY STAY APP");
+        System.out.println("     Hotel Booking System v1.0");
+        System.out.println("=================================");
+
+        System.out.println("Welcome to the Book My Stay Application!");
+        System.out.println("Your comfort is our priority.");
+
+    }
+}
